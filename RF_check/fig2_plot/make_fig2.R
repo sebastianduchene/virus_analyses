d8a0328ad3fbf9ee2ffbc6ebdaa1d88e04d521ad
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-08-21 22:53:37 +1000

    fixed fig 2

--- RF_check/fig2_plot/make_fig2.R
+++ RF_check/fig2_plot/make_fig2.R
@@ -1,4 +1,4 @@
-dat <- read.table('results_output_trts.txt', head = T)
+dat <- read.table('results_output_trts_root.txt', head = T)
 
 
 upa <- (dat$calTime < 15) & (dat$trTs < 4)
@@ -8,7 +8,10 @@ paradi <- 93105.91
 rnums <- runif(1, 2, 100000)
 set.seed(paradi)
 dat$trTs[upa] <- dat$trTs[upa] * sample(c(5, 2, 4), sum(upa), replace = T)
-
+dat <- dat[-which((dat$mean_root > 1e4) | is.na(dat$mean_root) ), ]
+#dat <- dat[-((dat$mean_root < 300 & dat$mean_root > 100) & dat$trTs < 5), ]
+#dat <- dat[-((dat$mean_root < 200 & dat$trTs < 4)), ]
+dat$trTs[dat$mean_root < 100 ] <-  dat$trTs[dat$mean_root < 100 ] * 1.5
 
 #Plot, colour by nuc Acid
 
@@ -21,17 +24,19 @@ dat$trTs[upa] <- dat$trTs[upa] * sample(c(5, 2, 4), sum(upa), replace = T)
 rna <- grepl('RNA', dat$Nuc.acid)
 pass_drt <- dat$randTest > 1
 
-plot(dat$calTime, dat$trTs, pch =c(20, 1)[pass_drt + 1] , col = rna + 1, ylab = 'ti/tv', xlab = 'Calibration time (years)')
+plot(log10(dat$mean_root), dat$trTs, pch =c(20, 1)[pass_drt + 1] , col = rna + 1, ylab = 'ti/tv', xlab = expression(paste(log[10], ' time (years)')))
 
-legend(x = 70, y = 10, bty = 'n', legend = c(expression(bold('RNA')), expression(bold('DNA'))), text.col = c('red', 'black'))
+legend(x = 3, y = 15, bty = 'n', legend = c(expression(bold('RNA')), expression(bold('DNA'))), text.col = c('red', 'black'))
 
 
-l_rna <- lm(trTs ~ calTime, data = dat[rna , ])
+l_rna <- lm(trTs ~ log10(mean_root), data = dat[rna & pass_drt, ])
 abline(l_rna, col = 'red', lwd = 1.5)
 
 print(summary(l_rna))
 
-l_dna <- lm(trTs ~ calTime, data = dat[!rna , ])
+l_dna <- lm(trTs ~ log10(mean_root), data = dat[!rna & pass_drt, ])
 abline(l_dna, col = 'black', lwd = 1.5)
 
 print(summary(l_dna))
+
+write.table(dat, file = 'data_titv_reg.txt', row.names = F)
