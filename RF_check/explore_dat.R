facb7b60b05978a0ad5ff04635ac169f687905c1
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-09-19 20:01:13 +1000

    added updated figs

--- RF_check/explore_dat.R
+++ RF_check/explore_dat.R
@@ -22,7 +22,7 @@ plot(log10(combine_dat$V12.x), combine_dat$V2.x, pch = 1:10, col = 'black', ylim
 points(log10(combine_dat$V12.y), combine_dat$V2.y, pch = 1:10, col = 'red', lwd = 1.5, cex = 1.5)
 legend(x = 3.2, y = 20, legend = c('ASFV', 'BYDV', 'CaPV', 'CYDV', 'DENV-4', 'EBOV', 'HBV', 'HIV-1', 'RaV', 'HIV-2+SIV' ), bty = 'n', cex = 0.7, pch = 1:10, pt.cex = 1.2)
 text(x = 1.1, y = 19.8, labels = expression(bold('A')), cex = 1.3)
-
+ 
 #legend(x = 2.8, y = 20, legend = gsub('[.]fasta', '', combine_dat$V1), bty = 'n', cex = 0.7, pch = 1:10)
 
 l_sig_titv <- c(1, 2)[grepl('CYDV|HIV_|rab', combine_dat$V1) + 1]
@@ -56,7 +56,7 @@ lines(x = c(log10(combine_dat$V12.x[i]), log10(combine_dat$V12.y[i])), y = c(com
 
 plot(log10(combine_dat$V12.x), combine_dat$cg.x, pch = 1:10, col =  'black', ylim = c(0.25, 0.55), xlim = c(1, 4), lwd = 1.5, ylab = 'CG-content', xlab = expression(paste('Root-node age (', log[10], ' years)')), , cex.lab = 1.2, cex = 1.5)
 points(log10(combine_dat$V12.y), combine_dat$cg.y, pch = 1:10, col = 'red', lwd = 1.5, cex = 1.5)
-text(x = 1.1, y = 1.09, labels = expression(bold('D')), cex = 1.3)
+text(x = 1.1, y = 0.545, labels = expression(bold('D')), cex = 1.3)
 
 
 for(i in 1:nrow(combine_dat)){
