cdac459076fea97633a2685c94e8affa245c35fc
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2015-05-27 16:33:08 +1000

    included x2 to table

--- flat_data/gc_empirical/mb_dpp/pps_moureau.R
+++ flat_data/gc_empirical/mb_dpp/pps_moureau.R
@@ -8,6 +8,40 @@ par(fg = 'white')
 par(col.lab = 'white')
 par(col.axis = 'white')
 
+
+
+
+
+mour_data <- rem_gaps(phyDat(read.nexus.data('Moureau_dpp.nexus'), type = 'AA'))
+mour_slen <- ncol(as.character(mour_data))
+
+# Model averaging of the Q matrix
+mour_ma_log <- read.table('../mb_runs/Moureau_mixed.p', head = T, skip = 1)
+mour_ma_tre <- read.nexus('../mb_runs/Moureau_mixed.t')
+
+s_ma <- pps_ma(mour_ma_log, mour_ma_tre, mour_slen, n_samples = 100)
+ml_ma <- sapply(s_ma, function(x) multlik(x))
+x2_ma <- sapply(s_ma, function(x) homogen_test(x))
+hist(ml_ma)
+hist(x2_ma)
+
+# Dirichlet prior on Q matrix
+mour_dpp_log <- read.table('Moureau_dpp.run1.p', head = T, skip = 1)
+mour_dpp_tre <- read.nexus('Moureau_dpp.run1.t')
+
+s_dpp <- pps_dpp(mour_dpp_log, mour_dpp_tre, mour_slen, n_samples = 100)
+ml_dpp <- sapply(s_dpp, function(x) multlik(x))
+x2_dpp <- sapply(s_dpp, function(x) homogen_test(x))
+hist(ml_dpp)
+hist(x2_dpp)
+
+stop('analysing Moureau')
+
+
+
+
+
+
 pet_data <- rem_gaps(phyDat(read.nexus.data('Petterson_dpp.nexus'), type = 'AA'))
 pet_slen <- ncol(as.character(pet_data))
 
@@ -33,6 +67,7 @@ hist(x2_dpp)
 
 
 stop('analysing Pettersson')
+
 
 hein_data <- rem_gaps(phyDat(read.nexus.data('Heinze_dpp.nexus'), type = 'AA'))
 hein_slen <- ncol(as.character(hein_data))
@@ -63,28 +98,6 @@ stop('analysing heinze')
 
 
 
-mour_data <- rem_gaps(phyDat(read.nexus.data('Moureau_dpp.nexus'), type = 'AA'))
-mour_slen <- ncol(as.character(mour_data))
-
-# Model averaging of the Q matrix
-mour_ma_log <- read.table('../mb_runs/Moureau_mixed.p', head = T, skip = 1)
-mour_ma_tre <- read.nexus('../mb_runs/Moureau_mixed.t')
-
-s_ma <- pps_ma(mour_ma_log, mour_ma_tre, mour_slen, n_samples = 10)
-ml_ma <- sapply(s_ma, function(x) multlik(x))
-x2_ma <- sapply(s_ma, function(x) homogen_test(x))
-hist(ml_ma)
-hist(x2_ma)
-
-# Dirichlet prior on Q matrix
-mour_dpp_log <- read.table('Moureau_dpp.run1.p', head = T, skip = 1)
-mour_dpp_tre <- read.nexus('Moureau_dpp.run1.t')
-
-s_dpp <- pps_dpp(mour_dpp_log, mour_dpp_tre, mour_slen, n_samples = 10)
-ml_dpp <- sapply(s_dpp, function(x) multlik(x))
-x2_dpp <- sapply(s_dpp, function(x) homogen_test(x))
-hist(ml_dpp)
-hist(x2_dpp)
 
 
 
