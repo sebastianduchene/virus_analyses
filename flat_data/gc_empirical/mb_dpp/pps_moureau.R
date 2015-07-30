50d7dc917cefffb9c738200404479cdfdeafa449
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2015-07-30 10:01:08 +1000

    updated flav runs

--- flat_data/gc_empirical/mb_dpp/pps_moureau.R
+++ flat_data/gc_empirical/mb_dpp/pps_moureau.R
@@ -12,6 +12,38 @@ par(col.axis = 'white')
 
 
 
+hein_data <- rem_gaps(phyDat(read.nexus.data('Heinze_dpp.nexus'), type = 'AA'))
+hein_slen <- ncol(as.character(hein_data))
+
+# Model averaging of the Q matrix
+hein_ma_log <- read.table('../mb_runs/Heinze_mixed.p', head = T, skip = 1)
+hein_ma_tre <- read.nexus('../mb_runs/Heinze_mixed.t')
+
+s_ma <- pps_ma(hein_ma_log, hein_ma_tre, hein_slen, n_samples = 100)
+ml_ma <- sapply(s_ma, function(x) multlik(x))
+x2_ma <- sapply(s_ma, function(x) homogen_test(x))
+hist(ml_ma)
+hist(x2_ma)
+
+# Dirichlet prior on Q matrix
+hein_dpp_log <- read.table('Heinze_dpp.run1.p', head = T, skip = 1)
+hein_dpp_tre <- read.nexus('Heinze_dpp.run1.t')
+
+s_dpp <- pps_dpp(hein_dpp_log, hein_dpp_tre, hein_slen, n_samples = 100)
+ml_dpp <- sapply(s_dpp, function(x) multlik(x))
+x2_dpp <- sapply(s_dpp, function(x) homogen_test(x))
+hist(ml_dpp)
+hist(x2_dpp)
+
+
+stop('analysing heinze')
+
+
+
+
+
+
+
 mour_data <- rem_gaps(phyDat(read.nexus.data('Moureau_dpp.nexus'), type = 'AA'))
 mour_slen <- ncol(as.character(mour_data))
 
@@ -67,34 +99,6 @@ hist(x2_dpp)
 
 
 stop('analysing Pettersson')
-
-
-hein_data <- rem_gaps(phyDat(read.nexus.data('Heinze_dpp.nexus'), type = 'AA'))
-hein_slen <- ncol(as.character(hein_data))
-
-# Model averaging of the Q matrix
-hein_ma_log <- read.table('../mb_runs/Heinze_mixed.p', head = T, skip = 1)
-hein_ma_tre <- read.nexus('../mb_runs/Heinze_mixed.t')
-
-s_ma <- pps_ma(hein_ma_log, hein_ma_tre, hein_slen, n_samples = 100)
-ml_ma <- sapply(s_ma, function(x) multlik(x))
-x2_ma <- sapply(s_ma, function(x) homogen_test(x))
-hist(ml_ma)
-hist(x2_ma)
-
-# Dirichlet prior on Q matrix
-hein_dpp_log <- read.table('Heinze_dpp.run1.p', head = T, skip = 1)
-hein_dpp_tre <- read.nexus('Heinze_dpp.run1.t')
-
-s_dpp <- pps_dpp(hein_dpp_log, hein_dpp_tre, hein_slen, n_samples = 100)
-ml_dpp <- sapply(s_dpp, function(x) multlik(x))
-x2_dpp <- sapply(s_dpp, function(x) homogen_test(x))
-hist(ml_dpp)
-hist(x2_dpp)
-
-
-stop('analysing heinze')
-
 
 
 
